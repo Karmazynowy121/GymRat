5d63b87655b860b0aee8fa9634d9f183f135e9a7
Karmazynowy121 <atak121@wp.pl>
2022-10-26 21:43:25 +0200

    Initial commit

---
+++ GymRat/TrainingPart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymRat
+{
+    public class TrainingPart
+    {
+        public List<Exercise> Exercise;
+        public int AmountPart;
+        public int AmountSeries;
+        public int BodyWeight;
+    }
+}
---
+++ GymRat/Trening.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymRat
+{
+    public class Trening
+    {
+       public List<TrainingPart> TrainingParts;
+       public string KindOfTraining;
+       public string WhichDayOfWeek;
+
+    }
+}
